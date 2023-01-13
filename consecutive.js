a5a16413f1f3ab8ba57ae1b04b59a13b31954ed7
mosh98 <mosleh.edu@gmail.com>
2023-01-13 10:49:30 +0100

    kata2

--- consecutive.js
+++ consecutive.js
@@ -1,28 +1,29 @@
 
 function consecutive_sum(n) {
     /**
-     *
-     * In summary, the algorithm is checking all possible consecutive number sequences starting from 1 to half of the input number
+     * In summary,
+     * the algorithm is checking all possible consecutive number sequences starting from 1 to half of the input number
      * and
      * if it finds any sum of consecutive numbers that is equal to the input number,
      * it returns true,
      * otherwise it returns false if it couldn't find any.*
      */
-
-
     for (let i = 1; i < (n/2) + 1; i++) {
 
-        let curr_sum = 0;
+        let curr_sum = 0; //addding stuffz
         //calculated by adding the current value of the inner loop variable to the sum
-        //
+        //lopoping (j<half of the number), because the added value will alawys be lower than ´n´
+
         for (let j = i; j < (n/2) + 2; j++) {
+
             curr_sum += j;
+
             if (curr_sum == n) {
                 return true;
             }
-            if (curr_sum > n) {
+            if (curr_sum > n)
+                //we don't want a value greater than n
                 break;
-            }
         }
     }
     return false;
@@ -30,5 +31,7 @@ function consecutive_sum(n) {
 
 
 
+
 console.log(consecutive_sum(64))
+
 
