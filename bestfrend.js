7b0c1834f1f0b99291890f4411cebeef81dbbe5d
mosh98 <mosleh.edu@gmail.com>
2023-01-17 09:27:12 +0100

    kata3

--- bestfrend.js
+++ bestfrend.js
@@ -1,17 +1,42 @@
-function bestfrend(sentence, charA, charB){
+function bestFriend(sentence, charA, charB){
+    /**
+     *finds a pairs of characters in a sentence
+     * @param {string} sentence A setence to control the pair
+     * @param {character} charA the first char
+     * @param {character} charB the secnond char
+     */
     wordList = sentence.split(" ")
 
-    sum = 0
+
+    sumA = 0
+    sumB = 0
+
     pattern = charA+charB
+
     for (let i = 0; i<wordList.length; i++ ){
-        //charList = wordList[i].split("")
-        if (wordList[i].includes(pattern)){
-            sum += 1
+        //in each word from wordList, CharA and CharB should occur together
+        charList = wordList[i].split("")
+
+        for (let j = 0; j < charList.length; j++){
+            if( charList[j] == charA ){
+                sumA += 1
+
+                if((j+1) < (charList.length-1)){
+                    if(charList[j+1] == charB){
+                        sumB += 1
+
+
+                    }
+                }
+            }
         }
+
+
     }
-    if (sum >= 2){
+    if (sumA == sumB){
         return true
     }
+
 
 
 
@@ -19,4 +44,5 @@ function bestfrend(sentence, charA, charB){
     return false
 }
 
-console.log(bestfrend("we found your dynamite", "d", "y"))
+console.log(bestFriend("we found your dynamite", "d", "y")) //False
+console.log(bestFriend("i found an ounce with my hound", "o", "u")) //True
